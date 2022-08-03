a6af867c8fa0e0e590d93927c4577db2631d3370
ForcedPush <34317796+ForcedPush@users.noreply.github.com>
2022-08-02 17:51:10 -0700

    project set up

---
+++ CaseStudy/CaseStudy/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  CaseStudy
+//
+//  Created by  ¯\_(ツ)_/¯ on 8/2/22.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
