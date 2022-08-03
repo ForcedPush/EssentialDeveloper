55d50e704481721b1a7e2f3bf524c7b688513095
ForcedPush <34317796+ForcedPush@users.noreply.github.com>
2022-08-02 18:02:58 -0700

    create feed framework

---
+++ CaseStudy/CaseStudy/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  CaseStudy
+//
+//  Created by  ¯\_(ツ)_/¯ on 8/2/22.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageURL: URL
+}
--- CaseStudy/CaseStudyTests/CaseStudyTests.swift
+++ CaseStudy/CaseStudyTests/CaseStudyTests.swift
@@ -6,6 +6,7 @@
 //
 
 import XCTest
+@testable import CaseStudy
 
 class CaseStudyTests: XCTestCase {
 
@@ -27,7 +28,7 @@ class CaseStudyTests: XCTestCase {
 
     func testPerformanceExample() throws {
         // This is an example of a performance test case.
-        measure {
+        self.measure {
             // Put the code you want to measure the time of here.
         }
     }
